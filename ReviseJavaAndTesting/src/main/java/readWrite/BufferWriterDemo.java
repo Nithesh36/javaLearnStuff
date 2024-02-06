29131e896007234735cc6290b747f1592c282237
Nithesh <nitheshkumar0731@gmail.com>
2024-02-06 07:42:32 +0530

    bufferwriter Class added

---
+++ ReviseJavaAndTesting/src/main/java/readWrite/BufferWriterDemo.java
@@ -0,0 +1,32 @@
+package readWrite;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferWriterDemo {
+
+	public static void main(String[] 	args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		/*it will not write  content directly to file (disc) 
+		 * it will lead to high performance
+		 * 
+		 * if we did not mention path file not already there 
+		 * it will create at Root level in project
+  */
+   FileWriter writer1=new FileWriter("BufferWriterFile.txt");
+   BufferedWriter bw=new BufferedWriter(writer1);
+   bw.write("nites");
+   bw.write("\n");
+   bw.write("se");
+   bw.close();
+   if( new BufferedReader(new FileReader("BufferWriterFile.txt")).lines().count()>0) {
+   System.out.println("file writing done");
+   
+   }
+	}
+
+}
--- ReviseJavaAndTesting/src/main/java/readWrite/FileWriterDemo.java
+++ ReviseJavaAndTesting/src/main/java/readWrite/FileWriterDemo.java
@@ -6,7 +6,11 @@ import java.io.IOException;
 public class FileWriterDemo {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+		
+		
+		/*it will  write  content directly to file (disc) 
+		 * it will lead to low performance
+  */
 		FileWriter writer=new FileWriter("writeFileUsingFileWriter.txt");
 		writer.write("helo");
 		writer.write("\n"); // write into new line using \n
