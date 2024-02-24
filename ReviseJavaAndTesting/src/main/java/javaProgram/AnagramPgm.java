9e5f9a2ea5b6a6ead7254e85a11398ad4717d293
Nithesh <nitheshkumar0731@gmail.com>
2024-02-24 12:33:34 +0530

    angram prgm added

---
+++ ReviseJavaAndTesting/src/main/java/javaProgram/AnagramPgm.java
@@ -0,0 +1,28 @@
+package javaProgram;
+
+public class AnagramPgm {
+public static void main(String[] args) {
+	String name1="hello";
+	String name2="holel";
+	boolean isAngram=false;
+	
+	if(name1.length()==name2.length())
+	for(int i=0;i<name1.length();i++) {
+		if(name1.contains(String.valueOf(name2.charAt(i)))) {
+			isAngram=true;
+		}
+		else
+		{
+			isAngram=false;
+			break;
+		}
+	}
+	
+	
+	if(isAngram)
+		System.out.println("amgaram");
+	else
+	System.out.println("not a angram");
+	
+}
+}
